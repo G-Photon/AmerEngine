efa9bb35c166723b754bbf56721a12c3656681b6
G-Photon <1103581526@qq.com>
2025-07-18 10:39:52 +0800

    整理删除项目

--- advancelight2/shader/deferred_shading_null.fs
+++
@@ -1,4 +0,0 @@
-#version 330 core
-void main() {
-    // 故意留空，不输出任何颜色
-}
--- advanceopengl/shader/Gouraud.fs
+++
@@ -1,7 +0,0 @@
-#version 330 core
-in vec3 Veccolor;
-out vec4 FragColor;
-void main()
-{
-    FragColor = vec4(Veccolor, 1.0);
-}
